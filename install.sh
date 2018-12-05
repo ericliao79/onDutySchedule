7ed01500d08b61d3889168b1b4c3a8aa773fbbcc
PhantasWeng <bensonuong@gmail.com>
2018-12-05 11:53:21 +0800

    Update preinstall script. check if screenshots folder exist

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 # echo '.'
-mkdir screenshots
-printf "\033[32m Do you want to install pm2 in this project? (y/N) \033[0m\n"
+if [ ! -d 'screenshots' ]; then
+  mkdir screenshots
+  printf "screenshots folder\033[37m created \033[0m.\n"
+fi
+printf "\033[32mDo you want to install pm2 in this project? (y/N) \033[0m\n"
 read option
 if [ "$option" = "N" ] || [ "$option" = "n" ] || [ "$option" = "" ]
   then
-    printf -- '\033[37m Passing installing of pm2 .... \033[0m\n'
+    printf -- '\033[37mPassing installing of pm2 .... \033[0m\n'
     printf -- '\n'
     # exit 1
   else
